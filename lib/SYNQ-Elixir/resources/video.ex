e6c249f75c0ee631537d100112e5885bbf73c249
Bruce Wang <byyw13@gmail.com>
2017-04-13 23:03:09 -0500

    fix docs (#12)
    * update doc
    
    * change to proper cdn
    
    * use json
    
    * put back circle install
    
    * comment out

--- lib/SYNQ-Elixir/resources/video.ex
+++ lib/SYNQ-Elixir/resources/video.ex
@@ -1,11 +1,11 @@
 defmodule SynqElixir.Resources.Video do
   @moduledoc """
   A video resource, am example metadata dump
-
+  ```json
   {
-    "video_id": "4e063ab632f24992aced5c6f8983229f",
+    "video_id": "video123",
     "input": {
-        "url": "https://dcuu5ylopkzzf.cloudfront.net/projects/1c/d7/1cd7494b9e8741e2852055e98486bc46/uploads/videos/4e/06/4e063ab632f24992aced5c6f8983229f.mp4",
+        "url": "https://multicdn.synq.fm/projects/1c/d7/1cd7494b9e8741e2852055e98486bc46/uploads/videos/4e/06/video123.mp4",
         "width": 1920,
         "height": 1080,
         "duration": "15.926000",
@@ -16,23 +16,24 @@ defmodule SynqElixir.Resources.Video do
     "state": "uploaded",
     "player": {
         "views": 0,
-        "embed_url": "https://player.synq.fm/embed/4e063ab632f24992aced5c6f8983229f"
+        "embed_url": "https://player.synq.fm/embed/video123"
+        "thumbnail_url": "https://multicdn.synq.fm/projects/0a/bf/0abfe1b849154082993f2fce77a16fd9/derivatives/thumbnails/4e/06/video123.jpg"
     },
     "outputs": {
         "hls": {
-            "url": "https://dcuu5ylopkzzf.cloudfront.net/projects/1c/d7/1cd7494b9e8741e2852055e98486bc46/derivatives/videos/4e/06/4e063ab632f24992aced5c6f8983229f/hls/4e063ab632f24992aced5c6f8983229f_hls.m3u8",
+            "url": "https://multicdn.synq.fm/projects/1c/d7/1cd7494b9e8741e2852055e98486bc46/derivatives/videos/4e/06/video123/hls/video123_hls.m3u8",
             "state": "complete"
         },
         "mp4_360": {
-            "url": "https://dcuu5ylopkzzf.cloudfront.net/projects/1c/d7/1cd7494b9e8741e2852055e98486bc46/derivatives/videos/4e/06/4e063ab632f24992aced5c6f8983229f/mp4_360/4e063ab632f24992aced5c6f8983229f_mp4_360.mp4",
+            "url": "https://multicdn.synq.fm/projects/1c/d7/1cd7494b9e8741e2852055e98486bc46/derivatives/videos/4e/06/video123/mp4_360/video123_mp4_360.mp4",
             "state": "complete"
         },
         "mp4_720": {
-            "url": "https://dcuu5ylopkzzf.cloudfront.net/projects/1c/d7/1cd7494b9e8741e2852055e98486bc46/derivatives/videos/4e/06/4e063ab632f24992aced5c6f8983229f/mp4_720/4e063ab632f24992aced5c6f8983229f_mp4_720.mp4",
+            "url": "https://multicdn.synq.fm/projects/1c/d7/1cd7494b9e8741e2852055e98486bc46/derivatives/videos/4e/06/video123/mp4_720/video123_mp4_720.mp4",
             "state": "complete"
         },
         "mp4_1080": {
-            "url": "https://dcuu5ylopkzzf.cloudfront.net/projects/1c/d7/1cd7494b9e8741e2852055e98486bc46/derivatives/videos/4e/06/4e063ab632f24992aced5c6f8983229f/mp4_1080/4e063ab632f24992aced5c6f8983229f_mp4_1080.mp4",
+            "url": "https://multicdn.synq.fm/projects/1c/d7/1cd7494b9e8741e2852055e98486bc46/derivatives/videos/4e/06/video123/mp4_1080/video123_mp4_1080.mp4",
             "state": "complete"
         },
         "webm_720": {
@@ -45,6 +46,7 @@ defmodule SynqElixir.Resources.Video do
     "created_at": "2016-08-01T15:41:51.555Z",
     "updated_at": "2016-10-09T19:37:10.281Z"
   }
+  ```
   """
 
   @derive [Poison.Encoder]
